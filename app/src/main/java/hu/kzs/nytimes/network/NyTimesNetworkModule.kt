ef2cf82290f02e05026cc58b9df542aa36eec4e7
zsof <61664782+zsof@users.noreply.github.com>
2021-10-20 13:27:49 +0200

    update gradle dependency versions, make HttpLogingInterceptor to log network calls

--- app/src/main/java/hu/kzs/nytimes/network/NyTimesNetworkModule.kt
+++ app/src/main/java/hu/kzs/nytimes/network/NyTimesNetworkModule.kt
@@ -1,5 +1,7 @@
 package hu.kzs.nytimes.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.create
@@ -16,8 +18,17 @@ fun provideNyTimesApi(retrofit: Retrofit): NyTimesApi = retrofit.create()
 
 fun provideRetrofit(): Retrofit {
 
+    val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val client: OkHttpClient = OkHttpClient.Builder().apply {
+        addInterceptor(interceptor)
+    }.build()
+
     return Retrofit.Builder()
         .baseUrl(NyTimesApi.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
         .build()
 }
